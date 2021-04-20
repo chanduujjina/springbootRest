4a1d834ed36539c4011c45f6dbcf0b8a4d212ae9
Anu Dharkar <anumig@gmail.com>
2021-04-20 10:55:55 -0500

    Added a comment

--- src/main/java/com/boot/rest/springbootRest/SpringbootRestApplication.java
+++ src/main/java/com/boot/rest/springbootRest/SpringbootRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootRestApplication {
-
+// Learning spring boot
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApplication.class, args);
 	}
